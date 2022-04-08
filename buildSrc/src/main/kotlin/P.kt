20a823435293d327982b50fc26c75790918d45b3
ForteScarlet <ForteScarlet@163.com>
2022-04-09 00:46:40 +0800

    试行新的版本规则

--- buildSrc/src/main/kotlin/P.kt
+++ buildSrc/src/main/kotlin/P.kt
@@ -1,34 +1,146 @@
+@file:Suppress("MemberVisibilityCanBePrivate", "unused")
 /*
- *  Copyright (c) 2022-2022 ForteScarlet <ForteScarlet@163.com>
- *
- *  本文件是 simbot-component-kaiheila 的一部分。
- *
- *  simbot-component-kaiheila 是自由软件：你可以再分发之和/或依照由自由软件基金会发布的 GNU 通用公共许可证修改之，无论是版本 3 许可证，还是（按你的决定）任何以后版都可以。
- *
- *  发布 simbot-component-kaiheila 是希望它能有用，但是并无保障;甚至连可销售和符合某个特定的目的都不保证。请参看 GNU 通用公共许可证，了解详情。
- *
- *  你应该随程序获得一份 GNU 通用公共许可证的复本。如果没有，请看:
- *  https://www.gnu.org/licenses
- *  https://www.gnu.org/licenses/gpl-3.0-standalone.html
- *  https://www.gnu.org/licenses/lgpl-3.0-standalone.html
- *
- *
- */
+*  Copyright (c) 2022-2022 ForteScarlet <ForteScarlet@163.com>
+*
+*  本文件是 simbot-component-kaiheila 的一部分。
+*
+*  simbot-component-kaiheila 是自由软件：你可以再分发之和/或依照由自由软件基金会发布的 GNU 通用公共许可证修改之，无论是版本 3 许可证，还是（按你的决定）任何以后版都可以。
+*
+*  发布 simbot-component-kaiheila 是希望它能有用，但是并无保障;甚至连可销售和符合某个特定的目的都不保证。请参看 GNU 通用公共许可证，了解详情。
+*
+*  你应该随程序获得一份 GNU 通用公共许可证的复本。如果没有，请看:
+*  https://www.gnu.org/licenses
+*  https://www.gnu.org/licenses/gpl-3.0-standalone.html
+*  https://www.gnu.org/licenses/lgpl-3.0-standalone.html
+*
+*
+*/
 
 
 object P {
     object Simbot {
-        val isSnapshot: Boolean = System.getProperty("isSnapshot")?.equals("true", true) ?: false
         const val GROUP = "love.forte.simbot"
-        const val REAL_VERSION = "3.0.0.preview.6.0"
-        val VERSION = if (isSnapshot) "$REAL_VERSION-SNAPSHOT" else REAL_VERSION
+
+        val version = Version(
+            major = "3",
+            minor = 0,
+            patch = 0,
+            status = preview(6, 0),
+            isSnapshot = System.getProperty("isSnapshot")?.equals("true", true) ?: false
+        )
+
+
+        val isSnapshot: Boolean get() = version.isSnapshot
+        // val REAL_VERSION = version.fullVersion(false)
+            // "3.0.0.preview.6.0"
+        val VERSION = version.fullVersion(true)
+        // = if (isSnapshot) "$REAL_VERSION-SNAPSHOT" else REAL_VERSION
     }
+
     object ComponentKaiheila {
         val isSnapshot: Boolean get() = Simbot.isSnapshot
         const val GROUP = "${Simbot.GROUP}.component"
+
+        val version = Version(
+            major = "${Simbot.version.major}.${Simbot.version.minor}",
+            minor = 0,
+            patch = 0,
+            status = preview(1, 0),
+            isSnapshot = isSnapshot
+        )
+
         // 0: v3 api
-        const val REAL_VERSION = "0.1.0"
-        val VERSION = if (isSnapshot) "${Simbot.REAL_VERSION}-$REAL_VERSION-SNAPSHOT"
-        else "${Simbot.REAL_VERSION}-$REAL_VERSION"
+        // const val REAL_VERSION = "3.0.1.0"
+
+        val VERSION = version.fullVersion(true)
     }
 }
+
+
+/**
+ * **P**roject **V**ersion。
+ */
+@Suppress("SpellCheckingInspection")
+data class Version(
+    /**
+     * 主版号
+     */
+    val major: String,
+    /**
+     * 次版号
+     */
+    val minor: Int,
+    /**
+     * 修订号
+     */
+    val patch: Int,
+
+    /**
+     * 状态号。状态号会追加在 [major].[minor].[patch] 之后，由 `.` 拼接，
+     * 变为 [major].[minor].[patch].[PVS.status].[PVS.minor].[PVS.patch].
+     *
+     * 例如：
+     * ```
+     * 3.0.0.preview.0.1
+     * ```
+     *
+     */
+    val status: PVS?,
+
+    /**
+     * 是否快照。如果是，将会在版本号结尾处拼接 `-SNAPSHOT`。
+     */
+    val isSnapshot: Boolean
+) {
+    companion object {
+        const val SNAPSHOT_SUFFIX = "-SNAPSHOT"
+    }
+
+    /**
+     * 没有任何后缀的版本号。
+     */
+    val standardVersion: String = "$major.$minor.$patch"
+
+
+    /**
+     * 完整的版本号。
+     */
+    fun fullVersion(checkSnapshot: Boolean): String {
+        return buildString {
+            append(major).append('.').append(minor).append('.').append(patch)
+            if (status != null) {
+                append('.').append(status.status).append('.').append(status.minor).append('.').append(status.patch)
+            }
+            if (checkSnapshot && isSnapshot) {
+                append(SNAPSHOT_SUFFIX)
+            }
+        }
+    }
+
+}
+
+/**
+ * **P**roject **V**ersion **S**tatus.
+ */
+@Suppress("SpellCheckingInspection")
+data class PVS(
+    val status: String,
+    /**
+     * 次版号
+     */
+    val minor: Int,
+    /**
+     * 修订号
+     */
+    val patch: Int,
+) {
+    companion object {
+        const val PREVIEW_STATUS = "preview"
+        const val BETA_STATUS = "beta"
+    }
+}
+
+
+internal fun preview(minor: Int, patch: Int) = PVS(PVS.PREVIEW_STATUS, minor, patch)
+
+
