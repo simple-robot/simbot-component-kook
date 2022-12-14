5c9f2d344a3ce8d3ae21ffba40539a92a69db5c7
ForteScarlet <ForteScarlet@163.com>
2022-12-14 21:19:51 +0800

    pref: component 序列化属性增加 JvmStatic

--- simbot-component-kook-core/src/main/kotlin/love/forte/simbot/component/kook/KookComponent.kt
+++ simbot-component-kook-core/src/main/kotlin/love/forte/simbot/component/kook/KookComponent.kt
@@ -41,58 +41,59 @@ public class KookComponent @InternalSimbotApi constructor() : Component {
      */
     override val id: String
         get() = ID_VALUE
-    
+
     /**
      * Kook 组件中所涉及到的序列化模块。
      *
      */
     override val componentSerializersModule: SerializersModule
         get() = messageSerializersModule
-    
+
     override fun toString(): String = TO_STRING_VALUE
-    
+
     override fun equals(other: Any?): Boolean {
         if (other === this) return true
         if (other !is KookComponent) return false
-        
+
         return id == other.id
     }
-    
+
     override fun hashCode(): Int {
         return id.hashCode()
     }
-    
+
     /**
      * 组件 [KookComponent] 的注册器。
      *
      */
     public companion object Factory : ComponentFactory<KookComponent, KookComponentConfiguration> {
-        
+
         /**
          * 组件 [KookComponent] 的ID
          */
         @Suppress("MemberVisibilityCanBePrivate")
         public const val ID_VALUE: String = "simbot.kook"
-        
+
         private const val TO_STRING_VALUE = "KookComponent(id=$ID_VALUE)"
-        
+
         /**
          * 组件的ID实例。
          */
         @Deprecated("Unused")
         public val componentID: CharSequenceID = ID_VALUE.ID
-        
+
         /**
          * 注册器的唯一标识。
          */
         override val key: Attribute<KookComponent> = attribute(ID_VALUE)
-        
+
         /**
          * [KookComponent] 组件所使用的消息序列化信息。
          *
          * _早期版本的 `khl.xx.xx` 格式的序列化模块请参考 [khlCompatibleMessageSerializersModule] _
          */
         @OptIn(ExperimentalSimbotApi::class)
+        @get:JvmStatic
         public val messageSerializersModule: SerializersModule = SerializersModule {
             fun PolymorphicModuleBuilder<KookMessageElement<*>>.include() {
                 subclass(KookSimpleAssetMessage::class, KookSimpleAssetMessage.serializer())
@@ -103,23 +104,23 @@ public class KookComponent @InternalSimbotApi constructor() : Component {
                 subclass(KookAttachmentImage::class, KookAttachmentImage.serializer())
                 subclass(KookAttachmentFile::class, KookAttachmentFile.serializer())
                 subclass(KookAttachmentVideo::class, KookAttachmentVideo.serializer())
-                
+
                 subclass(KookCardMessage::class, KookCardMessage.serializer())
                 subclass(KookKMarkdownMessage::class, KookKMarkdownMessage.serializer())
             }
             polymorphic(KMarkdown::class) {
                 subclass(RawValueKMarkdown::class, RawValueKMarkdown.serializer())
             }
-            
+
             polymorphic(KookMessageElement::class) {
                 include()
             }
-            
+
             polymorphic(Message.Element::class) {
                 include()
             }
         }
-    
+
         /**
          * 用于兼容 `khl.xx.xx` 更名为 `kook.xx.xx` 之前的消息序列化模组。
          *
@@ -135,7 +136,7 @@ public class KookComponent @InternalSimbotApi constructor() : Component {
             fun <T> KSerializer<T>.r(): KSerializer<T> {
                 return rename { it.replaceFirst("kook.", "khl.") }
             }
-            
+
             fun PolymorphicModuleBuilder<KookMessageElement<*>>.include() {
                 subclass(KookSimpleAssetMessage::class, KookSimpleAssetMessage.serializer().r())
                 subclass(KookAssetImage::class, KookAssetImage.serializer().r())
@@ -150,16 +151,16 @@ public class KookComponent @InternalSimbotApi constructor() : Component {
             polymorphic(KMarkdown::class) {
                 subclass(RawValueKMarkdown::class, RawValueKMarkdown.serializer().rename { "RAW_V_K_MD" }) // 曾经的序列化name为此。
             }
-    
+
             polymorphic(KookMessageElement::class) {
                 include()
             }
-    
+
             polymorphic(Message.Element::class) {
                 include()
             }
         }
-        
+
         /**
          * 构建一个 [KookComponent] 实例。
          */
@@ -169,7 +170,7 @@ public class KookComponent @InternalSimbotApi constructor() : Component {
             return KookComponent()
         }
     }
-    
+
 }
 
 /*
