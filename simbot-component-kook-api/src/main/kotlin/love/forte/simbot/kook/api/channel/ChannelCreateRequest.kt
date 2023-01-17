e37eca61276abf7525ce5ea4c931bf066d5c468f
ForteScarlet <ForteScarlet@163.com>
2023-01-17 17:41:38 +0800

    perf(api): ChannelCreateRequest 调整构建方式

--- simbot-component-kook-api/src/main/kotlin/love/forte/simbot/kook/api/channel/ChannelCreateRequest.kt
+++ simbot-component-kook-api/src/main/kotlin/love/forte/simbot/kook/api/channel/ChannelCreateRequest.kt
@@ -34,58 +34,86 @@ import love.forte.simbot.kook.objects.Channel
  *
  * @author ForteScarlet
  */
-public class ChannelCreateRequest @JvmOverloads constructor(
-    /** 是 服务器id */
+public class ChannelCreateRequest internal constructor(
     private val guildId: ID,
-    /** 是 频道名称 */
     private val name: String,
-    /** 否 频道类型，1 文字，2 语音，默认为文字 */
-    private val type: Int = 1,
-    /** 否 父分组id */
-    private val parentId: ID? = null,
-    /** 否 语音频道人数限制，最大99 */
-    private val limitAmount: Int = 99,
-    /** 否 语音音质，默认为2。1流畅，2正常，3高质量 */
-    private val voiceQuality: Int = 2,
+    private val type: Int,
+    private val parentId: ID?,
+    private val limitAmount: Int,
+    private val voiceQuality: Int,
 ) : KookPostRequest<Channel>() {
-    public companion object Key : BaseKookApiRequestKey("channel", "create")
-
+    @Suppress("MemberVisibilityCanBePrivate")
+    public companion object Key : BaseKookApiRequestKey("channel", "create") {
+        
+        /** 语音品质：流畅 */
+        public const val VOICE_QUALITY_SMOOTH: Int = 1
+        /** 语音品质：正常 */
+        public const val VOICE_QUALITY_NORMAL: Int = 2
+        /** 语音品质：高品质 */
+        public const val VOICE_QUALITY_HIGH: Int = 3
+        
+        
+        /**
+         * 构建 [ChannelCreateRequest].
+         * @param guildId 服务器id
+         * @param name 频道名称
+         * @param type 频道类型，1 文字，2 语音，默认为文字
+         * @param parentId 父分组id
+         * @param limitAmount 语音频道人数限制，最大99
+         * @param voiceQuality 语音音质，默认为2。
+         * - 流畅: 1（[VOICE_QUALITY_SMOOTH]）
+         * - 正常: 2（[VOICE_QUALITY_NORMAL]）
+         * - 高质量: 3（[VOICE_QUALITY_HIGH]）
+         */
+        @JvmOverloads
+        @JvmStatic
+        public fun create(
+            guildId: ID,
+            name: String,
+            type: Int = 1,
+            parentId: ID? = null,
+            limitAmount: Int = 99,
+            voiceQuality: Int = VOICE_QUALITY_NORMAL,
+        ): ChannelCreateRequest = ChannelCreateRequest(guildId, name, type, parentId, limitAmount, voiceQuality)
+        
+    }
+    
     init {
         require(type in 1..2) { "The 'type' must be 1(Text) or 2(Voice), but $type" }
         require(limitAmount in 1..99) { "The 'limitAmount' must between 1 and 99, but $limitAmount" }
         require(voiceQuality in 1..3) { "The 'voiceQuality' must between 1 and 3, but $voiceQuality" }
     }
-
+    
     override val resultDeserializer: DeserializationStrategy<out Channel>
         get() = ChannelView.serializer()
-
+    
     override val apiPaths: List<String>
         get() = apiPathList
-
+    
     override fun createBody(): Any = Body(guildId, name, type, parentId, limitAmount, voiceQuality)
-
+    
     @Serializable
     private data class Body(
         /** 是 服务器id */
         @SerialName("guild_id")
         @Serializable(ID.AsCharSequenceIDSerializer::class)
         val guildId: ID,
-
+        
         /** 是 频道名称 */
         val name: String,
-
+        
         /** 否 频道类型，1 文字，2 语音，默认为文字 */
         val type: Int = 1,
-
+        
         /** 否 父分组id */
         @SerialName("parent_id")
         @Serializable(ID.AsCharSequenceIDSerializer::class)
         val parentId: ID? = null,
-
+        
         /** 否 语音频道人数限制，最大99 */
         @SerialName("limit_amount")
         val limitAmount: Int = 99,
-
+        
         /** 否 语音音质，默认为2。1流畅，2正常，3高质量 */
         @SerialName("voice_quality")
         val voiceQuality: Int = 2,
