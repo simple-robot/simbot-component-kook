113fbf234d53dee5b26baecbf2076bcb69e9578c
ForteScarlet <ForteScarlet@163.com>
2023-04-07 23:31:40 +0800

    reconstruct: 重构的准备，但是真的要重构吗

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -26,7 +26,7 @@ repositories {
 
 val kotlinVersion = "1.7.20"
 val dokkaVersion = "1.8.10"
-val suspendTransformVersion = "0.0.5"
+val suspendTransformVersion = "0.2.7"
 val gradleCommon = "0.0.11"
 
 dependencies {
