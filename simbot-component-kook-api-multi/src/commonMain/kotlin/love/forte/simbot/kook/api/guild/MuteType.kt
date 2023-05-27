af25e234066552bb0f7ac11c18e9aeec3bf0474a
ForteScarlet <ForteScarlet@163.com>
2023-05-27 16:00:35 +0800

    API: 删除服务器静音或闭麦

---
+++ simbot-component-kook-api-multi/src/commonMain/kotlin/love/forte/simbot/kook/api/guild/MuteType.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023. ForteScarlet.
+ *
+ * This file is part of simbot-component-kook.
+ *
+ * simbot-component-kook is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * simbot-component-kook is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with simbot-component-kook,
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package love.forte.simbot.kook.api.guild
+
+/**
+ * 静音类型
+ *
+ * @see CreateGuildMuteApi
+ * @see DeleteGuildMuteApi
+ */
+public enum class MuteType(internal val value: Int) {
+    /**
+     * 麦克风闭麦
+     */
+    MIC(1),
+
+    /**
+     * 耳机静音
+     */
+    HEADSET(2)
+}
