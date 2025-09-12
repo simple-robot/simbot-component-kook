49dfaa95a305d3e4a71652c9cf90dd1b7e8f1f27
ForteScarlet <ForteScarlet@163.com>
2025-09-12 18:03:40 +0800

    feat(api): implement KookChannel updator and extend channel properties

---
+++ simbot-component-kook-core/src/commonMain/kotlin/love/forte/simbot/component/kook/internal/KookChannelUpdatorImpl.kt
@@ -0,0 +1,51 @@
+/*
+ *     Copyright (c) 2025. ForteScarlet.
+ *
+ *     This file is part of simbot-component-kook.
+ *
+ *     simbot-component-kook is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     simbot-component-kook is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with simbot-component-kook,
+ *     If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package love.forte.simbot.component.kook.internal
+
+import love.forte.simbot.common.id.literal
+import love.forte.simbot.component.kook.KookChannel
+import love.forte.simbot.component.kook.KookChannelUpdator
+import love.forte.simbot.component.kook.bot.internal.KookBotImpl
+import love.forte.simbot.component.kook.util.requestDataBy
+import love.forte.simbot.kook.api.channel.UpdateChannelApi
+import love.forte.simbot.kook.api.channel.toChannel
+
+/**
+ * @author ForteScarlet
+ */
+internal class KookChannelUpdatorImpl(override val channel: KookChannel, private val bot: KookBotImpl) :
+    KookChannelUpdator {
+    override var builder: UpdateChannelApi.Builder = UpdateChannelApi.builder(channel.id.literal)
+
+    override suspend fun execute(): KookChannel {
+        val api = builder.build()
+        val result = api.requestDataBy(channel.bot)
+        val resultChannel = result.toChannel()
+
+        val updatedChannel = if (result.isCategory) {
+            resultChannel.toCategoryChannel(bot, resultChannel.toCategory(bot, null))
+        } else {
+            resultChannel.toChatChannel(bot, null)
+        }
+
+        return updatedChannel
+    }
+}
